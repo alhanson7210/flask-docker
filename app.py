d6206f4473319d25639d8c2fe1c2013740706e27
Jason Hao <jhao4@dons.usfca.edu>
2021-04-21 17:38:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+  print('Hello world from console')
   return 'Hello from the other side of docker!'
 
-
+# Im editing in github as well
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
